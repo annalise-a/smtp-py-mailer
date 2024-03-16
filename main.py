20736e6fb1de300e871f08066477c7c8e98f2167
annalise-a <150473341+annalise-a@users.noreply.github.com>
2024-03-16 16:03:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import tkinter as tk
 from tkinter import messagebox
 
 def send_email():
-    # obtain values from entry fields
     email_sender = sender_entry.get()
     email_password = password_entry.get()
     email_receiver = receiver_entry.get()
@@ -16,7 +15,6 @@ def send_email():
     subject = subject_entry.get()
     body = body_entry.get("1.0",'end').strip()
 
-    # Validate email addresses
     if not is_valid_email(email_sender):
         messagebox.showerror("Error", "Invalid sender email address")
         return
@@ -30,7 +28,6 @@ def send_email():
         messagebox.showerror("Error", "Invalid Bcc email address")
         return
     
-    # creates an emailMessage object
     em = EmailMessage()
     em['From'] = email_sender
     em['To'] = email_receiver
@@ -42,24 +39,18 @@ def send_email():
     context = ssl.create_default_context()
 
     try:
-        #connect to SMTP server
         with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-            # login to google mail
             smtp.login(email_sender, email_password)
-
-            # send the email
             smtp.sendmail(email_sender, recipients, em.as_string())
         messagebox.showinfo("Success", f'email sent successfully!')
         clear_window()
     except Exception as e:
         messagebox.showerror("Error",f'an error has occured: {e}')
 
-# Regular expression pattern for validating email addresses
 def is_valid_email(email):
     pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
     return re.match(pattern, email) is not None
 
-# function to clear fields after successfully sending mail
 def clear_window():
     receiver_entry.delete(0, 'end')
     subject_entry.delete(0, 'end')
@@ -67,12 +58,9 @@ def clear_window():
     bcc_entry.delete(0, 'end')
     body_entry.delete("1.0", 'end')
 
-
-# Create the main application window
 root = tk.Tk()
 root.title("Email Sender")
 
-# Create entry fields and labels
 sender_label = tk.Label(root, text="User Email:")
 sender_label.grid(row=0, column=0)
 sender_entry = tk.Entry(root)
@@ -108,13 +96,10 @@ body_label.grid(row=6, column=0)
 body_entry = tk.Text(root, wrap="word", height=10)
 body_entry.grid(row=6, column=1, sticky="nsew")
 
-# allows row to expand vertically with more text
 root.grid_rowconfigure(6, weight=1)
 
 send_button = tk.Button(root, text="Send", command=send_email)
 send_button.grid(row=7, columnspan=2)
 
-
-# Tkinter event loop to open window
 root.mainloop()
 
